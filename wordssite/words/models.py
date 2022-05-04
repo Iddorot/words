b6fc12507edf251ac86271c3b90ef78cb1fed380
Ido Rot <idorot9768@gmail.com>
2022-05-04 09:23:27 +0000

    delete choice model

--- wordssite/words/models.py
+++ wordssite/words/models.py
@@ -9,13 +9,6 @@ class Word(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.word_text
-        def was_published_recently(self):
-            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-
-class Choice(models.Model):
-    word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
