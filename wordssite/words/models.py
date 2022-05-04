53957bda5912611691e4fdccd4bad4e3bb7f5f45
Ido Rot <idorot9768@gmail.com>
2022-05-04 08:00:31 +0000

    add words app to admin

--- wordssite/words/models.py
+++ wordssite/words/models.py
@@ -1,12 +1,21 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Word(models.Model):
     word_text = models.CharField(max_length=200)
+    word_translation = models.CharField(max_length=200, default='DEFAULT VALUE')
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.word_text
+        def was_published_recently(self):
+            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-class Translation(models.Model):
+class Choice(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    translation_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
