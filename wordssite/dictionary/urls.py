52b0544384a782dbb663d7b2a13763484f8e0b42
Ido Rot <idorot9768@gmail.com>
2022-05-11 07:17:18 +0000

    Word CRUD class based done

--- wordssite/dictionary/urls.py
+++ wordssite/dictionary/urls.py
@@ -5,7 +5,7 @@ app_name = 'dictionary'
 
 urlpatterns = [
     path('', views.WordListView.as_view(), name='all'),
-    re_path('dictionary/<uuid:pk>/detail', views.WordDetailView.as_view(), name='word_detail'),
+    path('dictionary/<uuid:pk>/detail', views.WordDetailView.as_view(), name='word_detail'),
     path('dictionary/create/', views.WordCreateView.as_view(), name='word_create'),
     path('dictionary/<uuid:pk>/update/', views.WordUpdateView.as_view(), name='word_update'),
     path('dictionary/<uuid:pk>/delete/', views.WordDeleteView.as_view(), name='word_delete'),
