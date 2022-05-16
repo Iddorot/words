25ddec5f417ed20b0c24e3f753085761aad332f6
Ido Rot <idorot9768@gmail.com>
2022-05-16 07:42:23 +0000

    delete window.location from templates

--- wordssite/dictionary/urls.py
+++ wordssite/dictionary/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('dictionary/create/', views.WordCreateView.as_view(), name='word_create'),
     path('dictionary/<uuid:pk>/update/', views.WordUpdateView.as_view(), name='word_update'),
     path('dictionary/<uuid:pk>/delete/', views.WordDeleteView.as_view(), name='word_delete'),
-
-    path('', views.TranslationListView.as_view(), name='all'),
+    
     path('dictionary/translation/create/', views.TranslationCreateView.as_view(), name='translation_create'),
     path('dictionary/translation/<uuid:pk>/update/', views.TranslationUpdateView.as_view(), name='translation_update'),
     path('dictionary/translation/<uuid:pk>/delete/', views.TranslationDeleteView.as_view(), name='translation_delete'),
