f810e79a5d8f381cdb9745c9560173efe738e624
Ido Rot <89927111+Iddorot@users.noreply.github.com>
2022-06-14 16:50:08 +0300

    Add new transltion at the word_detail template (#7)
    * work on inline formset
    
    * wip formset
    
    * update word detail view to use get method
    
    * add basic view tests
    
    * fix word detail template
    
    * investigate formset request
    
    * add translation form to formset view
    
    * add separate function for post
    
    * add formset.error
    
    * add word instance to formset
    
    * delete translation create view and url
    
    Co-authored-by: Dziugas <dziugas.tornau@gmail.com>

--- wordssite/dictionary/views.py
+++ wordssite/dictionary/views.py
@@ -3,9 +3,13 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.forms import inlineformset_factory
+from django.http import HttpResponseRedirect
+
 from .models import Word,Translation
 from .forms import TranslationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 
 
@@ -18,27 +22,29 @@ class WordBaseView(View):
 class WordListView(WordBaseView, ListView):
     """View to list all Word"""
 
-class WordDetailView(WordBaseView, DetailView):
-    def word_translation(request, word_id):
-        word = get_object_or_404(models.word, id=Word_id)
-        word_form = forms.WordForm(instance=word)
-        translation_form = forms.TranslationForm()
-        if request.method == 'POST':
-            if 'word_translation' in request.POST:
-                word_form = forms.wordForm(request.POST, instance=word)
-                if word_form.is_valid():
-                    word_form.save()
-                    return redirect('home')
-            if 'translation_form' in request.POST:
-                translation_form = forms.TranslationForm(request.POST)
-                if translation_form.is_valid():
-                    word.translation()
-                    return redirect('home')
-        context = {
-            'word_form': word_form,
-            'translation_form': translation_form,
-        }
-        return render(request, 'dictionary/word_detail.html', context=context)
+class WordDetailView(WordBaseView, UpdateView):
+     
+    def get(self, request, pk):
+        WordFormSet = inlineformset_factory(Word, Translation, fields= ('translation', 'language'), extra = 1)
+        word = Word.objects.get(pk=pk)  
+        formset = WordFormSet(instance=word,queryset=Word.objects.none())
+        context = {'formset':formset, 'word': word}       
+        return render(request, 'dictionary/word_detail.html', context)
+    
+    def post(self, request, pk):
+        WordFormSet = inlineformset_factory(Word, Translation, fields= ('translation', 'language'), extra = 1) 
+        word = Word.objects.get(pk=pk)
+        formset = WordFormSet(request.POST, instance=word)
+        context = {'formset':formset, 'word': word}
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect("")
+
+        else:
+            print(f"Formset is invalid, errors:\n {formset.errors}")
+
+        return render(request,'dictionary/word_detail.html', context)
+
     """View to list the details from one Word"""
 
 class WordCreateView(WordBaseView, CreateView):
@@ -56,20 +62,6 @@ class TranslationBaseView(View):
     model = Translation
     fields = 'translation', 'language'
     success_url = reverse_lazy('dictionary:all')   
-
-
-class TranslationCreateView(TranslationBaseView, CreateView):
-    def post(self, request, pk):
-        if request.method == 'POST':
-            form=TranslationForm(request.POST)
-            if form.is_valid():
-                translation = form.save(commit=False)
-                word = Word.objects.get(pk=pk)
-                translation.word= word
-                translation.save()
-        form=TranslationForm()
-        return render(request,'dictionary/word_detail.html', {'form':form, 'word':word})   
-    """View to create a new Translation"""
 
 class TranslationUpdateView(TranslationBaseView, UpdateView):
     """View to update a Translation"""
