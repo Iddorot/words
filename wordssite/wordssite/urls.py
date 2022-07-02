490b116148398b4ed98ef361a514ee3e2507a529
Ido Rot <idorot9768@gmail.com>
2022-07-02 19:02:02 +0000

    wip show words as fliping cards

--- wordssite/wordssite/urls.py
+++ wordssite/wordssite/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('dictionary.urls')),
-    
+       
 ]
