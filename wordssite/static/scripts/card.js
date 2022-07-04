9f5fbf493b89b8c3fd039d2f7df9147b191c19d9
Ido Rot <idorot9768@gmail.com>
2022-07-04 11:45:34 +0000

    flip cards on click

--- wordssite/static/scripts/card.js
+++ wordssite/static/scripts/card.js
@@ -1,7 +1,13 @@
-var cards = document.querySelectorAll('.card');
+console.log("are we loading?")
+function flip_card(cards) {
+  cards.forEach((card)=>{
+    card.addEventListener( 'click', function(){
+      card.classList.toggle('is-flipped');
+    });
+  })}
 
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-  });
-});
+window.onload = () => {
+  console.log("yes")
+  var cards = document.querySelectorAll('.card');
+  flip_card(cards)
+};
