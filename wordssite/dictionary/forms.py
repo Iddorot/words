ecf34ea8fc8c312c8ea067a3078603b479cb739e
Ido Rot <idorot9768@gmail.com>
2022-07-27 09:31:45 +0000

    more readable clean function in translation formset

--- wordssite/dictionary/forms.py
+++ wordssite/dictionary/forms.py
@@ -49,17 +49,19 @@ class TranslationFormset(inlineformset_factory(
             },
         )):
 
-    def clean(self):
-        if any(self.errors):
-            return
-            
-        if not any(self.cleaned_data):
-            return
-            
-        word =self.instance.id
-        if self.cleaned_data:
-            translation = self.cleaned_data[0]['translation']
-            if Translation.objects.filter(translation=translation,word = word).exists():
-                raise ValidationError(f"{translation} already exist")
 
-        return translation
+    def clean(self):
+        def check_if_exists(self):
+            word =self.instance.id
+            if self.cleaned_data:
+                translation = self.cleaned_data[0]['translation']
+                if Translation.objects.filter(translation=translation,word = word).exists():
+                    raise ValidationError(f"{translation} already exist")
+
+        if any(self.errors):
+                raise ValidationError ("please fill the needed data")
+        if not any(self.cleaned_data):
+                raise ValidationError ("please fill the needed data")
+
+        if check_if_exists(self):
+            return translation
