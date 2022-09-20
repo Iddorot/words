15269ff8cb3c176b72cf1f088573d641ba6145fe
Ido Rot <idorot9768@gmail.com>
2022-09-20 21:54:50 +0300

    fix urls.py and translation_confirm_delete.html

--- wordssite/wordssite/urls.py
+++ wordssite/wordssite/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("dictionary.urls")),
-<<<<<<< HEAD
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('dictionary/', include('dictionary.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
-=======
->>>>>>> main
 ]
